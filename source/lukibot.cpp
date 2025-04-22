6f0bf482c6b9d79962b348fecc53c40d2688e83f
lipski21 <lukasz.lipski.wro@gmail.com>
2025-04-22 20:11:30 +0200

    added simple hand evaluation in lukibot

--- source/lukibot.cpp
+++ source/lukibot.cpp
@@ -8,6 +8,53 @@ Lukibot::Lukibot(int initialStack) : Player(initialStack) {}
 
 Action Lukibot::makeAction(int raiseMoney, int pot, int tableButton) {
 
+    int handScore = 0;
+    int colorScore = 5;
+    int pairScore = 10;
+    int cardsConnected = 4;
+    int cardsLongConnected = 3;
+    int cardsVeryLongConnected = 1;
+    int highCardScore = 2;
+    int midCardScore = 1;
+    int aceBonus = 1;
+
+    // hand bonuses
+
+    if (hand[0].getColour() == hand[1].getColour()) handScore += colorScore;
+    if (hand[0].getNumber() == hand[1].getNumber()) handScore += pairScore;
+
+    int higherCardValue = std::max(hand[0].getNumber(), (hand[1].getNumber()));
+    int lowerCardValue = std::min(hand[0].getNumber(), (hand[1].getNumber()));
+
+    if (higherCardValue - 1 == lowerCardValue) handScore += cardsConnected;
+    else if (higherCardValue - 2 == lowerCardValue) handScore += cardsLongConnected;
+    else if (higherCardValue - 3 == lowerCardValue) handScore += cardsVeryLongConnected;
+
+
+    if (hand[0].getNumber() >= 12) handScore += highCardScore;
+    else if (hand[0].getNumber() >= 8) handScore += midCardScore;
+    if (hand[1].getNumber() >= 12) handScore += highCardScore;
+    else if (hand[1].getNumber() >= 8) handScore += midCardScore;
+
+    if (hand[0].getNumber() == 14) handScore += aceBonus;
+    if (hand[1].getNumber() == 14) handScore += aceBonus;
+
+    if (lowerCardValue == 2 &&
+        higherCardValue == 7 &&
+        hand[0].getColour() != hand[1].getColour()) handScore += 3;
+
+
+
+    // placement
+
+
+
+    // stack, pot itp.
+
+    
+
+
+
     if(stack<=0){
         setAction(PASS);
         return PASS;
